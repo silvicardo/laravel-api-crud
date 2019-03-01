d5ef265a6932cdb55aa6990df82ac05849de32dc
Riccardo Silvi <silvicardo@gmail.com>
2019-03-01 16:39:11 +0100

    addCategoryToProducts migration

--- app/Product.php
+++ app/Product.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = ['name','description','quantity'];
-    
+
+    public function category(){
+      return $this->belongsTo('App\Category');
+    }
+
 }
---
+++ database/migrations/2019_03_01_153207_add_category_to_products.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCategoryToProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->unsignedInteger('category_id')->after('id');
+            $table->foreign('category_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+}
